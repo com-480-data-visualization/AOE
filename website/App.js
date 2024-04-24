befdc26d8d53d067ad493a73d6c2aba8ef263009
OSqalli <othmane.sqallihoussaini@epfl.ch>
2024-04-24 23:24:26 +0200

    Add improvements to skeleton of website

--- website/App.js
+++ website/App.js
@@ -1,26 +1,35 @@
+
 const allRonds = document.querySelectorAll('.rond');
 const allBoxes = document.querySelectorAll('.box');
 
-const controller = new ScrollMagic.Controller()
+const controller = new ScrollMagic.Controller();
+
+document.addEventListener("scroll", function() {
+    var scrollPosition = window.scrollY;
+    var links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        var section = document.querySelector(link.hash);
+        if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
+    });
+});
 
 allBoxes.forEach(box => {
-
-    for(i = 0; i < allRonds.length; i++){
-
-        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
-
-            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
-
+    for (let i = 0; i < allRonds.length; i++) {
+        if (allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')) {
+            let tween = gsap.from(box, { y: -50, opacity: 0, duration: 0.5 });
             let scene = new ScrollMagic.Scene({
                 triggerElement: allRonds[i],
                 reverse: true
             })
             .setTween(tween)
+            //Debug
             // .addIndicators()
-            .addTo(controller)
-
+            .addTo(controller);
         }
-
     }
-
-})
+});
